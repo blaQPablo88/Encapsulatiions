e09aa76492da0298d27d6cc13b7de948e65957a8
kamogotjhb024 <kamogotjhb024@student.wethinkcode.co.za>
2025-02-26 23:20:53 +0200

    created class

--- 01.basics/src/Main.java
+++ 01.basics/src/Main.java
@@ -1,5 +1,16 @@
+
+// Following Telusko @https://www.youtube.com/watch?v=YbqneqDIZh8
+class Human {
+    int age = 22;
+    String name;
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        Human human = new Human();
+        age = human.age;
+        System.out.println(age);
     }
 }
