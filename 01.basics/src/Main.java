ea9e5c2eb96a6f18d578323247ba3127ee139f80
kamogotjhb024 <kamogotjhb024@student.wethinkcode.co.za>
2025-02-26 23:29:25 +0200

    privatised modifiers

--- 01.basics/src/Main.java
+++ 01.basics/src/Main.java
@@ -1,16 +1,26 @@
 
 // Following Telusko @https://www.youtube.com/watch?v=YbqneqDIZh8
+
 class Human {
-    int age = 22;
-    String name;
+    private int age = 22;
+    private String name = "Kagiso";
+
+    public int getAge() {
+        return age;
+    }
+    public String getName() {
+        return name;
+    }
+
 }
 
 public class Main {
     public static void main(String[] args) {
 
-
         Human human = new Human();
-        age = human.age;
-        System.out.println(age);
+        int age = human.getAge();
+        String name = human.getName();
+        System.out.println(name + " : " + age);
+
     }
 }
