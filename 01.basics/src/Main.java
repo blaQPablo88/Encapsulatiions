3f4ad7f6b6ddcdd652da020fb05fa864f7412b0f
kamogotjhb024 <kamogotjhb024@student.wethinkcode.co.za>
2025-02-26 23:38:17 +0200

    using get and set

--- 01.basics/src/Main.java
+++ 01.basics/src/Main.java
@@ -2,15 +2,18 @@
 // Following Telusko @https://www.youtube.com/watch?v=YbqneqDIZh8
 
 class Human {
-    private int age = 22;
-    private String name = "Kagiso";
+    private int age;
+    private String name;
 
     public int getAge() {
         return age;
     }
+    public void setAge(int a) {age = a;}
+
     public String getName() {
         return name;
     }
+    public void setName(String n) {name = n;}
 
 }
 
@@ -18,6 +21,8 @@ public class Main {
     public static void main(String[] args) {
 
         Human human = new Human();
+        human.setAge(22);
+        human.setName("blaQPablo88");
         int age = human.getAge();
         String name = human.getName();
         System.out.println(name + " : " + age);
